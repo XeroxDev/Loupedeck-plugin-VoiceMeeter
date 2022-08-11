5b4434765b108b8c6719fdd60c7bf8cc739e4f09
T.T <tt3333@tt-server.net>
2022-08-11 20:55:43 +0900

    feat: Load command support

---
+++ VoiceMeeterPlugin/Commands/LoadCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Loupedeck.VoiceMeeterPlugin.Commands
+{
+    using System;
+    using System.IO;
+
+    using Bases;
+
+    using Helper;
+
+    using Library.Voicemeeter;
+
+    public class LoadCommand : PluginDynamicCommand
+    {
+        public LoadCommand() : base("Load", "Load settings", "Special")
+        {
+            this.MakeProfileAction("text;Path:");
+        }
+
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+            => DrawingHelper.DrawDefaultImage("Load", Path.GetFileNameWithoutExtension(actionParameter) ?? "", ColorHelper.Inactive);
+
+        protected override void RunCommand(String actionParameter) => Remote.Load(actionParameter);
+    }
+}
